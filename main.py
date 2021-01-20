07c473f9717b4c6bf9521aaabbb1788f9a10e6ff
Nathaniel  Fisher <nathanielfisher@u.boisestate.edu>
2021-01-19 21:37:06 -0700

    Added main and functions folder

---
+++ main.py
@@ -0,0 +1,10 @@
+import functions.HNNop as HNNop
+import numpy as np
+
+class ANN():
+
+    def __init__(self,inputNodes,outputNodes):
+        self.__inputNodes = inputNodes
+        self.__outputNodes = outputNodes
+
+
